5dce0a47965040aaf82c6f86eea9f3cde7d59c00
Lucena02 <riku.lucena@hotmail.com>
2023-02-19 17:59:04 +0000

    TPC1 concluido

--- TPC1/res.py
+++ TPC1/res.py
@@ -6,10 +6,8 @@ col = []
 batimento = []
 doença = []
 
-
 def parsing(ficheiro) :
     f = open(ficheiro, "r")
-
     for line in f :
         linhaSep = line.split(",")
         idade.append(linhaSep[0])
@@ -19,7 +17,27 @@ def parsing(ficheiro) :
         batimento.append(linhaSep[4])
         doença.append((linhaSep[5])[:-1])
 
-        
+
+        # AUXILIARES -----------------------------------------
+
+def divide(a,b):
+    if (b == 0):
+        return 0
+    else: return a/b
+
+def maxi(lista):
+    maximo = 0
+    for x in lista:
+        try:
+            if int(x) > maximo:
+                maximo = int(x)
+        except ValueError:
+            pass
+    return maximo
+
+
+        # QUERY 1 ---------------------------------------------
+
 def sexoDoença():
     indice = 0
     m = 0
@@ -29,17 +47,26 @@ def sexoDoença():
             if (sexo[indice] == "M"): m += 1
             else: f += 1
         indice += 1
-    print("Doentes do sexo Masculino: " + str(m) + "\n" + "Doentes do sexo Feminino: " + str(f))
-        
 
-def divide(a,b):
-    if (b == 0):
-        return 0
-    else: return a/b
+    return (m,f)
+    
+
+
+def drawSexoDoença(t):
+    print(" Doentes de acordo com o seu sexo")
+    print(" Masculino   Feminino")
+    print(" " + str(t[0]) + " " * (12-(len(str(t[0])))) + str(t[1]) + " " * (10-(len(str(t[1])))))
+
+
+
+
+        # QUERY 2 ---------------------------------------------
 
 def ageRange():
-    list = dict.fromkeys(range(18), 0)
-    total = dict.fromkeys(range(18), 0)
+    maximo = int(maxi(idade))
+    maximo = int(maximo/5 + 1)
+    list = dict.fromkeys(range(maximo), 0)
+    total = dict.fromkeys(range(maximo), 0)
     indice = 0
     for age in idade:
         if (age == "idade") : pass
@@ -48,33 +75,68 @@ def ageRange():
                 list[int((int(age))/5)] += 1
             total[int((int(age))/5)] += 1
         indice += 1
-    
-    print("Doentes de acordo com a sua faixa etária:\n")
-    for i in range(18):
-        print(str(i*5) + "-" + str((i+1)*5-1) + ": " + str(list[i]) + " de " + str(total[i]) + " doentes -> " + str(divide(list[i],total[i])*100) + "%")
 
+    return (list, total, maximo)
+    
+    
+
+def drawAgeRange(list):
+    print("\nDoentes de acordo com a sua faixa etária:\n \n")
+
+    print(" Faixa etária    Nº Doentes    Nº Saudaveis    Percentagem ")
+    for i in range(list[2]):
+        print(" " + str(i*5) + "-" + str((i+1)*5-1) + " " * (15 - (len(str(i*5)) + len(str((i+1)*5-1))))  + str(list[0][i]) + " " * (14 - len(str(list[0][i])))  + str(list[1][i]) + " " * (16 - len(str(list[1][i]))) + str(round(divide(list[0][i],list[1][i])*100, 2)) + "%")
+
+
+        # QUERY 3 ---------------------------------------------
 
 def colesterolRange():
-    list = dict.fromkeys(range(61), 0)
-    total = dict.fromkeys(range(61), 0)
+    maximo = int(maxi(col))
+    maximo = int(maximo/10 + 1)
+    list = dict.fromkeys(range(maximo), 0)
+    total = dict.fromkeys(range(maximo), 0)
     indice = 0
     for colesterol in col:
-        print(colesterol)
         if (colesterol == "colesterol") : pass
         else :
             if (doença[indice] == "1") :
                 list[int((int(colesterol))/10)] += 1
             total[int((int(colesterol))/10)] += 1
         indice += 1
+
+    return(list,total,maximo)
     
-    print("Doentes de acordo com o seu colesterol:\n")
-    for i in range(61):
-        print(str(i*10) + "-" + str(i*10+10) + ": " + str(list[i]) + " de " + str(total[i]) + " doentes -> " + str(divide(list[i],total[i])*100) + "%")
+    
+
+
+def drawColesterolRange(list):
+    print("\nDoentes de acordo com o seu nível de colesterol:\n \n")
+
+    print(" Colesterol    Nº Doentes    Nº Saudaveis    Percentagem ")
+    #for i in range(maximo):
+        #print(str(i*10) + "-" + str(i*10+10) + ": " + str(list[i]) + " de " + str(total[i]) + " doentes -> " + str(divide(list[i],total[i])*100) + "%")
+    for i in range(list[2]):
+        print(" " + str(i*10) + "-" + str(i*10+10) + " " * (13 - (len(str(i*10)) + len(str(i*10+10))))  + str(list[0][i]) + " " * (14 - len(str(list[0][i])))  + str(list[1][i]) + " " * (16 - len(str(list[1][i]))) + str(round(divide(list[0][i],list[1][i])*100, 2)) + "%")
+
+        # MAIN
 
 def main():
     parsing("myheart.csv")
-    #sexoDoença()
-    #ageRange()
-    colesterolRange()
+    opcao = 0
+    while opcao != 4 :
+        print("[1] Doentes por sexo \n[2] Doentes por idades\n[3] Doentes por colesterol\n[4] Sair")
+        opcao = int(input("Selecione uma opcao:"))
+        if opcao == 1:
+            drawSexoDoença(sexoDoença())
+        elif opcao == 2:
+            drawAgeRange(ageRange())
+        elif opcao == 3:
+            drawColesterolRange(colesterolRange())
+        elif opcao == 4:
+            print("A encerrar...")
+            break
+        else:
+            print("Comando nao reconhecido")
+
 
 main()
