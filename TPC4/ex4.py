2dbc25821fcf8cc062699573b595eb2a7ad0b8fb
Lucena02 <riku.lucena@hotmail.com>
2023-03-13 17:11:55 +0000

    Quase acabado

--- TPC4/ex4.py
+++ TPC4/ex4.py
@@ -6,30 +6,40 @@ def parseFicheiro(file):
     
     f = open(file,"r", encoding='utf-8')
     j = open(file[:-4] + ".json", "w", encoding='utf-8')
+    flag = 0
 
     result = re.findall("(\w+)(\d+|\{\d+\}|\{\d+,\d+\})?(\:\:\w+)?", f.readline())
     print(result)
 
-    regex = ""
+    regex = "^"
     for resultado in result:
-        regex += f"(?P<{resultado[0]}>[\w\s]+"
+        regex += f"(?P<{resultado[0]}>" + "([\w\s]+"
+        flag = 0
         if resultado[1] != "":
             print(resultado[1])
-            for i in range (int(resultado[1][1]) - 1):
-                regex += f",[\w\s]+"
+            regex += f"\,?){resultado[1]}(?:,*)?"
+            flag = 1
+            #for i in range (int(resultado[1][1]) - 1):
+                #regex += f",[\w\s]+"
 
-        regex += "),"
+        if flag == 1:
+            regex += "),"
+        else:
+            regex += ")),"
 
     
     
     regex = regex [:-1]
+    regex += "$"
     print(regex)
 
     linhas = f.readlines()
 
     for linha in linhas:
+        print(linha)
         print("OI")
         resultt = re.match(regex,linha)
+        print(resultt)
 
         dictG = resultt.groupdict()
 
@@ -38,18 +48,14 @@ def parseFicheiro(file):
             if (len(listaa) > 1):
                 listatemp = []
                 for elemento in listaa:
-                    listatemp.append(elemento)
+                    if elemento != "":
+                        listatemp.append(elemento)
                 dictG[param] = listatemp
                 print("aiaiai")
 
 
         json.dump(dictG, j, ensure_ascii=False, indent = 4)
 
-
-
-        
-
-    #json.dump(dict, j, ensure_ascii=False, indent = 4)
 
 
 parseFicheiro("alunos.csv")
