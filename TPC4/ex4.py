78b660c212c22f693cb776cb14445e9fd6f8ef95
Lucena02 <riku.lucena@hotmail.com>
2023-03-13 23:35:06 +0000

    TPC4 Concluido

--- TPC4/ex4.py
+++ TPC4/ex4.py
@@ -1,17 +1,33 @@
 import json
 import re
 
+def sumat(lista):
+    soma = 0
+    for e in lista: soma += int(e) 
+    return soma 
+
+def mediat(lista):
+    soma = 0
+    t = 0
+    for e in lista: 
+        soma += int(e) 
+        t += 1
+    return soma/t
 
 def parseFicheiro(file):
     
     f = open(file,"r", encoding='utf-8')
     j = open(file[:-4] + ".json", "w", encoding='utf-8')
     flag = 0
+    functs = {}
+    i = 0
+    regex = "^"
+
 
     result = re.findall("(\w+)(\d+|\{\d+\}|\{\d+,\d+\})?(\:\:\w+)?", f.readline())
     print(result)
 
-    regex = "^"
+    
     for resultado in result:
         regex += f"(?P<{resultado[0]}>" + "([\w\s]+"
         flag = 0
@@ -19,30 +35,26 @@ def parseFicheiro(file):
             print(resultado[1])
             regex += f"\,?){resultado[1]}(?:,*)?"
             flag = 1
-            #for i in range (int(resultado[1][1]) - 1):
-                #regex += f",[\w\s]+"
+        if resultado[2] != "":
+            functs[i] = resultado[2][2:]
 
         if flag == 1:
             regex += "),"
         else:
             regex += ")),"
+        i += 1
 
-    
     
     regex = regex [:-1]
     regex += "$"
     print(regex)
-
     linhas = f.readlines()
 
     for linha in linhas:
-        print(linha)
-        print("OI")
         resultt = re.match(regex,linha)
-        print(resultt)
-
         dictG = resultt.groupdict()
 
+        i = 0
         for param in dictG:
             listaa = dictG[param].split(",")
             if (len(listaa) > 1):
@@ -50,12 +62,14 @@ def parseFicheiro(file):
                 for elemento in listaa:
                     if elemento != "":
                         listatemp.append(elemento)
+                if (i in functs):
+                    if functs[i] == "sum":
+                        listatemp = sumat(listatemp)
+                    elif functs[i] == "media":
+                        listatemp = mediat(listatemp)
                 dictG[param] = listatemp
-                print("aiaiai")
-
+            i += 1
 
         json.dump(dictG, j, ensure_ascii=False, indent = 4)
-
-
 
 parseFicheiro("alunos.csv")
